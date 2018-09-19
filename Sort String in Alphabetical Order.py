2d2774b6695bbaf3d1217c9e1ed0161a6f99f50f
sejall <33982474+sejall@users.noreply.github.com>
2018-09-19 07:03:57 +0530

    Add files via upload

---
+++ Sort String in Alphabetical Order.py
@@ -0,0 +1,9 @@
+# Python Program - Sort String in Alphabetical Order
+
+print("Enter 'x' for exit.");
+string = input("Enter any string to sort in alphabetical order: ")
+if string == 'x':
+    exit();
+else:
+    print("\nString after sorted in alphabetical order:");
+    print(''.join(sorted(string)));
